71afe46b01de21b03c0b20bbc97e0dc13d3e14ab
Genevieve Schultz <genschultz@gmail.com>
2018-10-21 23:36:04 -0700

    Delete gatsby-node.js

--- build/dist/gatsby-node.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-// You can delete this file if you're not using it
-//# sourceMappingURL=gatsby-node.js.map
