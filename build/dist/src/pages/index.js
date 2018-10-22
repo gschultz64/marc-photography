f2610f24df18335526bb52cb2d36ebbc22448245
Genevieve Schultz <genschultz@gmail.com>
2018-10-21 23:45:56 -0700

    Delete index.js

--- build/dist/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { Link } from 'gatsby';
-import Layout from '../components/layout';
-var IndexPage = function () { return (React.createElement(Layout, null,
-    React.createElement(Link, { to: "/about/" }, "Go to About Page"),
-    React.createElement("br", null),
-    React.createElement(Link, { to: "/store/" }, "Go to Store Page"))); };
-export default IndexPage;
-//# sourceMappingURL=index.js.map
